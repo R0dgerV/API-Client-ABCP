e0c2d50000c17f2a3009544b6698a890e83f554f
Andrey Bunyaev <abunyaev@gmail.com>
2016-11-10 22:53:41 +0300

    add .gitignore

---
+++ src/ApiClient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace R0dgerV\ApiClientABCP;
+
+/**
+ * Class ApiClientABCP
+ * @package R0dgerV\ApiClientABCP
+ */
+class ApiClient
+{
+    public function __construct()
+    {
+        echo 1;
+    }
+}
