3fbe6180b99fd2b8edbd9f7167a40721a618ed31
Andrey Bunyaev <abunyaev@gmail.com>
2016-11-12 10:34:24 +0300

    add search/articles method

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # API-Client-ABCP
+
+[API abcp.ru] - Документация по API
+
+В данном классе реализован только метод *Поиск детали по номеру и бренду*
+
+url: search/articles
+
+Пример использования 
+
+> $user - Имя пользователя
+
+> $password - Пароль в md5
+
+>host - http://имя.сайта.public.api.abcp.ru/
+            
+```php
+        $service = new R0dgerV\ApiClientABCP\ApiClient(
+            $user,
+            $password,
+            $host
+        );
+        $data = [
+            'number'=> '0242245571',
+            'brand'=>'Bosch'
+        ];
+        $results = $service->searchArticles($data);
+```
+
+
+[API abcp.ru]: http://docs.abcp.ru/wiki/API:Docs
+
--- src/ApiClient.php
+++ src/ApiClient.php
@@ -2,14 +2,91 @@
 
 namespace R0dgerV\ApiClientABCP;
 
+use GuzzleHttp\Client;
+
 /**
  * Class ApiClientABCP
- * @package R0dgerV\ApiClientABCP
+ * @package R0dgerV\ApiClient
  */
 class ApiClient
 {
-    public function __construct()
+
+    /**
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var
+     */
+    protected $baseUrl;
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @param string $baseUrl
+     */
+    public function __construct($username, $password, $baseUrl)
     {
-        echo 1;
+        $this->username = $username;
+        $this->password = $password;
+        $this->baseUrl = $baseUrl;
     }
+
+    /**
+     * @param string $url
+     * @param array $data
+     * @return array
+     */
+    protected function getQuery($url, array $data)
+    {
+        $response = $this->getClient()->request('GET', $url, [
+            'headers' => [
+                'User-Agent' => 'rodger-api-client/1.0',
+                'Accept'     => 'application/json',
+            ],
+            'query' => $data
+        ]);
+
+        return \GuzzleHttp\json_decode($response->getBody());
+    }
+
+    /**
+     * @return Client
+     */
+    protected function getClient()
+    {
+        if (!$this->client) {
+            $this->client = new Client(['base_uri' => $this->baseUrl]);
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    public function searchArticles(array $data)
+    {
+        $data = array_merge($data,
+            [
+                'userlogin' => $this->username,
+                'userpsw' => $this->password,
+            ]
+        );
+
+        return $this->getQuery('search/articles', $data);
+    }
+
 }
